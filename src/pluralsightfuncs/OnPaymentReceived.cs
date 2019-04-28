d3948c85a623d906ae4e8f3bd38b90ab3dde6cb2
Jim Andreasen <reifnir@gmail.com>
2019-04-28 17:13:44 -0400

    ready to test

--- src/pluralsightfuncs/OnPaymentReceived.cs
+++ src/pluralsightfuncs/OnPaymentReceived.cs
@@ -1,9 +1,11 @@
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace pluralsightfuncs
 {
@@ -12,16 +14,22 @@ namespace pluralsightfuncs
         [FunctionName("OnPaymentReceived")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
-            [Queue("orders")] IAsyncCollector<Order> orderQueue,
+            [Queue("orders"/*, Connection="MyAppSetting"*/)] IAsyncCollector<Order> orderQueue,
             ILogger log)
             {
                 log.LogInformation("Received a payment.");
-                //continue...
-                return null;
+
+                var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                var order = JsonConvert.DeserializeObject<Order>(requestBody);
+                await orderQueue.AddAsync(order);
+                log.LogInformation($"Order {order.OrderId} received from {order.Email} for product {order.ProductId}");
+                return new OkObjectResult($"Thank you for your purchase");
             }
     }
     public class Order
     {
-        
+        public string ProductId { get; set; }
+        public string Email { get; set; }
+        public string OrderId { get; set; }
     }
 }
