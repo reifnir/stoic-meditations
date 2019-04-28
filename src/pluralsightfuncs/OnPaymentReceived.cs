3fe5a4c22299fe9481ec6b4aa8c6c214072dd6bb
Jim Andreasen <reifnir@gmail.com>
2019-04-28 16:54:55 -0400

    start of an Azure function

---
+++ src/pluralsightfuncs/OnPaymentReceived.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace pluralsightfuncs
+{
+    public static class OnPaymentReceived
+    {
+        [FunctionName("OnPaymentReceived")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [Queue("orders")] IAsyncCollector<Order> orderQueue,
+            ILogger log)
+            {
+                log.LogInformation("Received a payment.");
+                //continue...
+                return null;
+            }
+    }
+    public class Order
+    {
+        
+    }
+}
